da5b6de8ad771823219e113a51daca0172dcde4a
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2022-06-22 07:43:04 +0000

    copilot

--- src-articlevoting/testSubmitOrder.cs
+++ src-articlevoting/testSubmitOrder.cs
@@ -10,12 +10,18 @@ using Newtonsoft.Json;
 
 namespace JJ.Function
 {
-    public class Order
+    public class JJOrderReq
     {
         public string id { get; set; }
         public string address { get; set; }
     }
-
+    
+    public class JJOrderDocument
+    {
+        public string uniqueId { get; set; }
+        public string id { get; set; }
+        public string address { get; set; }
+    }
 
     // test: curl -X POST http://localhost/api/testSubmitOrder -H "Content-Type: application/json" -d '{ \"id\": \"1\", \"address\": \"CZ\" }'
     public static class SubmitOrder
@@ -27,15 +33,15 @@ namespace JJ.Function
             databaseName: "jjdb",
             collectionName: "orders", CreateIfNotExists = true,
             ConnectionStringSetting = "jjcosmos_orders_DOCUMENTDB")]
-            ICollector<Order> ordersOut,
+            ICollector<JJOrderDocument> ordersOut,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string body = await req.ReadAsStringAsync();
-            var order = JsonConvert.DeserializeObject<Order>(body);
+            var order = JsonConvert.DeserializeObject<JJOrderReq>(body);
 
-            ordersOut.Add(order);
+            // use Copilot to save the order to CosmosDB (ordersOut.Add)
 
             return new OkObjectResult("Order created.");
         }
